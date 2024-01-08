e8c374daece76a655de3a42ad9447ab08f337dc0
vitthalkokare <vitthalkokare777@gmail.com>
2024-01-08 18:32:52 +0530

    client

---
+++ client/src/app/(routes)/auth/Login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        This is login page
+      
+    </div>
+  )
+}
+
+export default page
---
+++ client/src/app/types.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+
+export interface Country{
+    icon: ReactNode,
+    country: string,
+    code: number,
+
+}
